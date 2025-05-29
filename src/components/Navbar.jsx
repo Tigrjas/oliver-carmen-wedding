0c459a8d74c5d70233516da709ce9f476c7fc44e
Jason Tran <jasontran893code@gmail.com>
2025-05-29 16:52:32 -0700

    feat: dynamic day countdown to wedding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 export default function Navbar() {
+  const WEDDING_DATE = new Date('2026-5-10');
+  const currentDate = new Date();
+  const daysUntilWedding = daysBetween(WEDDING_DATE, currentDate);
+
   return (
     <nav className="navbar">
-        <h1 className='navbar-title'>Oliver and Carmen</h1>
+        <h1 className='navbar-title'>The Wedding of Oliver and Carmen</h1>
         <div className="navbar-date-info">
-            <p className="navbar-date">5/10/2026</p>
-            <p className="navbar-date-countdown">5 days to go</p>
+            <p className="navbar-date">May 10, 2026 • Reno, NV</p>
+            <p className="navbar-date-countdown">{daysUntilWedding} days to go!</p>
         </div>
     </nav>
   )
+}
+
+function daysBetween(date1, date2) {
+  const timeDiff = date2.getTime() - date1.getTime();
+  const daysDiff = Math.round(timeDiff / (1000 * 60 * 60 * 24));
+  return Math.abs(daysDiff);
 }
